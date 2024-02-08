c3708a0117e9d6a6d3133ee68407c18270adeb3a
Foskcru <enzo.desbois69@icloud.com>
2024-02-08 11:32:50 +0100

    0.3

--- ToolAnchor/ToolAnchor/MainWindow.xaml.cs
+++ ToolAnchor/ToolAnchor/MainWindow.xaml.cs
@@ -26,7 +26,9 @@ namespace ToolAnchor
         public MainWindow()
         {
             InitializeComponent();
-            ResizeMode = ResizeMode.NoResize;
+            //ResizeMode = ResizeMode.CanMinimize;
+            Topmost = true;
+            WidgetRichTextBox.Document.Blocks.Clear(); 
         }
 
         float widthRatio = -10;
@@ -70,10 +72,29 @@ namespace ToolAnchor
             if (MinimumX == -10 || MinimumY == -10 || MaximumX == -10 || MaximumY == -10) return;
             if (offsetLeft == -10 || offsetTop == -10 || offsetRight == -10 || offsetBottom == -10) return;
 
-            string _minimumX = (MinimumX + (offsetLeft * widthRatio)).ToString();
-            string _minimumY = (MinimumY + (offsetTop * heightRatio)).ToString();
-            string _maximumX = (MaximumX - (offsetRight * widthRatio)).ToString();
-            string _maximumY = (MaximumY - (offsetBottom * heightRatio)).ToString();
+            string _minimumX = "";
+            string _minimumY = "";
+            string _maximumX = "";
+            string _maximumY = "";
+            if (MinimumX != 0 || MinimumY != 0 || MaximumX != 0 || MaximumY != 0)
+            {
+                _minimumX = (MinimumX + (offsetLeft * widthRatio)).ToString();
+                _minimumY = (MinimumY + (offsetTop * heightRatio)).ToString();
+                _maximumX = (MaximumX - (offsetRight * widthRatio)).ToString();
+                _maximumY = (MaximumY - (offsetBottom * heightRatio)).ToString();
+            }
+            else
+            {
+                float _minX = offsetLeft * widthRatio;
+                float _minY = offsetTop * heightRatio;
+                float _maxX = offsetRight * widthRatio;
+                float _maxY = offsetBottom * heightRatio;
+                
+                _minimumX = (_minX).ToString();
+                _minimumY = (_minY).ToString();
+                _maximumX = (_maxX + _minX).ToString();
+                _maximumY = (_maxY + _minY).ToString();
+            }
 
             AnchorAdjustText.Text = "(Minimum=(X=" + _minimumX + ",Y=" + _minimumY + "),Maximum=(X=" + _maximumX + ",Y=" + _maximumY + "))";
         }
@@ -93,58 +114,7 @@ namespace ToolAnchor
                 heightRatio = 1 / float.Parse(HeightText.Text);
             SetAnchorAdjust();
         }
-
-        private void AnchorsText_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ChangeVisibility(AnchorsText, DefaultAnchors);
-
-            string pattern = @"\(Minimum=\(X=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Y=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\),Maximum=\(X=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Y=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\)\)";
-            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
-
-            if (regex.IsMatch(AnchorsText.Text))
-            {
-                string[] _value = regex.Split(AnchorsText.Text);
-
-                MinimumX = float.Parse(_value[1]);
-                MinimumY = float.Parse(_value[2]);
-                MaximumX = float.Parse(_value[3]);
-                MaximumY = float.Parse(_value[4]);
-            }
-            SetAnchorAdjust();
-        }
-
-        private void OffsetLeftText_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ChangeVisibility(OffsetLeftText, DefaultOffsetLeft);
-            if (!string.IsNullOrEmpty(OffsetLeftText.Text) && float.TryParse(OffsetLeftText.Text, out _))
-                offsetLeft = float.Parse(OffsetLeftText.Text);
-            SetAnchorAdjust();
-        }
-
-        private void OffsetTopText_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ChangeVisibility(OffsetTopText, DefaultOffsetTop);
-            if (!string.IsNullOrEmpty(OffsetTopText.Text) && float.TryParse(OffsetTopText.Text, out _))
-                offsetTop = float.Parse(OffsetTopText.Text);
-            SetAnchorAdjust();
-        }
-
-        private void OffsetRightText_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ChangeVisibility(OffsetRightText, DefaultOffsetRight);
-            if (!string.IsNullOrEmpty(OffsetRightText.Text) && float.TryParse(OffsetRightText.Text, out _))
-                offsetRight = float.Parse(OffsetRightText.Text);
-            SetAnchorAdjust();
-        }
-
-        private void OffsetBottomText_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            ChangeVisibility(OffsetBottomText, DefaultOffsetBottom);
-            if (!string.IsNullOrEmpty(OffsetBottomText.Text) && float.TryParse(OffsetBottomText.Text, out _))
-                offsetBottom = float.Parse(OffsetBottomText.Text);
-            SetAnchorAdjust();
-        }
-
+        
         private void AnchorAdjustText_TextChanged(object sender, TextChangedEventArgs e)
         {
             ChangeVisibility(AnchorAdjustText, DefaultAnchorAdjust);
@@ -168,21 +138,53 @@ namespace ToolAnchor
 
             //WidthText.Text = "";
             //HeightText.Text = "";
-            AnchorsText.Text = "";
-            OffsetLeftText.Text = "";
-            OffsetTopText.Text = "";
-            OffsetRightText.Text = "";
-            OffsetBottomText.Text = "";
+            
+            WidgetRichTextBox.Document.Blocks.Clear(); 
+            
             AnchorAdjustText.Text = "";
-;
-
         }
 
         private void Copy_Click(object sender, RoutedEventArgs e)
         {
-            //Clipboard.SetDataObject("test");
-            //if (!string.IsNullOrEmpty(AnchorAdjustText.Text))
-            //    Clipboard.SetText(AnchorAdjustText.Text);
+            if (!string.IsNullOrEmpty(AnchorAdjustText.Text))
+                Clipboard.SetDataObject(AnchorAdjustText.Text);
+        }
+        
+        private void WidgetRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (WidgetRichTextBox.Document.Blocks.Count() > 0)
+            {
+                if(DefaultWidget != null)
+                    DefaultWidget.Visibility = Visibility.Collapsed;
+            }
+            else if (WidgetRichTextBox.Document.Blocks.Count()  == 0)
+            {
+                if(DefaultWidget != null)
+                    DefaultWidget.Visibility = Visibility.Visible;
+            }
+            
+            TextRange source = new TextRange(WidgetRichTextBox.Document.ContentStart, WidgetRichTextBox.Document.ContentEnd);
+            
+            const string pattern = @"\(Offsets=\(Left=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Top=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Right=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Bottom=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\),Anchors=\(Minimum=\(X=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Y=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\),Maximum=\(X=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?,Y=([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?\)\)";
+            Regex _regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            if (_regex.IsMatch(source.Text))
+            {
+                string[] _value = _regex.Split(source.Text);
+                if (_value.Count() >= 9)
+                {
+                    offsetLeft = float.Parse(_value[1]);
+                    offsetTop = float.Parse(_value[2]);
+                    offsetRight = float.Parse(_value[3]);
+                    offsetBottom = float.Parse(_value[4]);
+                    
+                    MinimumX = float.Parse(_value[5]);
+                    MinimumY = float.Parse(_value[6]);
+                    MaximumX = float.Parse(_value[7]);
+                    MaximumY = float.Parse(_value[8]);
+                }
+            }
+            SetAnchorAdjust();
         }
     }
 }
