747bd846c494c7c2c3238c378f5fbdfadd43115c
Foskcru <enzo.desbois69@icloud.com>
2024-02-09 16:31:17 +0100

    0.4

--- ToolAnchor/ToolAnchor/obj/Release/ToolAnchor_Content.g.i.cs
+++ ToolAnchor/ToolAnchor/obj/Release/ToolAnchor_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ico.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("userwidget_64-svg-ico.ico")]
 
 
